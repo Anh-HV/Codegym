624990e7d829ddf424eaf12b585ccbfb659bc430
0911643618 <vietanh123456123@gmail.com>
2021-03-17 11:04:19 +0700

    them file

--- Module1/Module1_l13/BtArray02/js.js
+++ Module1/Module1_l13/BtArray02/js.js
@@ -24,3 +24,50 @@ function clickb2() {
     arr.reverse();
     console.log(arr.toString());
 }
+function clickb3() {
+    let value =parseInt(prompt("Nhap so luong phan tu cua mang",""));
+    let arr=[];
+    let count=0;
+    let x="";
+    for(let i=0;i<value;i++){
+        let a=prompt("Nhap Phan tu thu: "+(i+1),"");
+        arr[i]=a;
+    }
+    for(let i=0;i<arr.length;i++){
+        if(parseFloat (arr[i])>0 || parseFloat (arr[i]<0)) {
+            x+=arr[i];
+            count++;
+        }
+    }
+    console.log("So phan tu la so:"+count)
+}
+function clickb4() {
+    let value =parseInt(prompt("Nhap so luong phan tu cua mang",""));
+    let arr=[];
+    let count=0;
+    let x="";
+    for(let i=0;i<value;i++){
+        let a=prompt("Nhap Phan tu thu: "+(i+1),"");
+        arr[i]=a;
+    }
+    for(let i=0;i<arr.length;i++){
+        if(parseFloat (arr[i])>0 || parseFloat (arr[i]<0)) {
+            x+=arr[i];
+            count++;
+        }
+    }
+    console.log("So phan tu la chu:"+(arr.length-count));
+}
+function clickb5(){
+    let str1 = prompt("Nhap chuoi thu 1");
+    let str2 =prompt("Nhap chuoi thu 2");
+    if(str1.localeCompare(str2)==0){
+        alert("Chuoi 1 bang chuoi 2");
+    }
+    else alert("Chuoi 1 khac chuoi 2")
+}
+function clickb6(){
+    let str1 = prompt("Nhap chuoi thu 1");
+    console.log(str1);
+    console.log(str1.replaceAll("_","-"))
+}
